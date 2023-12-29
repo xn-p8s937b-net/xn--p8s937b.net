bd6debe1f1e84edcc5114f45cf905cfe26397ec6
Lee0701 <hgl1002@naver.com>
2023-12-29 19:09:58 +0900

    Add punycode processing

--- update.js
+++ update.js
@@ -1,20 +1,23 @@
 
 require('dotenv').config()
 const cloudflare = require('cloudflare')
-
+const punycode = require('punycode/')
 const fs = require('fs/promises')
 
 const { CF_TOKEN, CF_ZONE_ID } = process.env
+
+const keysToPunycode = (records) => Object.fromEntries(Object.entries(records)
+    .map(([key, value]) => [punycode.toASCII(key), value]))
 
 async function main() {
     const cf = cloudflare({
         token: CF_TOKEN,
     })
-    const local = JSON.parse(await fs.readFile('records.json'))
+    const local = keysToPunycode(JSON.parse(await fs.readFile('records.json')))
     const response = await cf.dnsRecords.browse(CF_ZONE_ID)
     const filtered = response.result
         .filter((record) => record.type === 'CNAME' && record.comment === 'github')
-    const remote = Object.fromEntries(filtered.map((record) => [record.name.split('.')[0], record.content]))
+    const remote = keysToPunycode(Object.fromEntries(filtered.map((record) => [record.name.split('.')[0], record.content])))
 
     const localKeys = new Set(Object.keys(local))
     const remoteKeys = new Set(Object.keys(remote))
